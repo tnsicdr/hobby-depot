85a0bf03440df95bd9c9514a302e8cedf74f1a74
tensei <908739+tnsicdr@users.noreply.github.com>
2026-04-09 00:02:25 -0400

    chore: add vitest runner

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
   "stories": [
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [],
+  "addons": ["@storybook/addon-vitest"],
   "framework": "@storybook/react-vite"
 };
 export default config;
